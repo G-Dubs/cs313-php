6d7ae61f4f4ea79b48e8099df8a644f538be4563
George <wie15003@byui.edu>
2019-04-30 16:39:25 -0600

    Changed the NavBar

--- web/NavBar.php
+++ web/NavBar.php
@@ -1,42 +1,41 @@
 <?php
-    echo "<header> 
-            <nav>
-                <ul>
-                    <li>
-                        <a href = \"home.php\">
-                            <tag>
-                                <tagtag>
-                                    <button type = button>
-                                        Distract Me Not Industries LLC.
-                                    </button> 
-                                </tagtag>
-                            </tag>
-                        </a>
-                    </li>
-                    <li>
-                        <a href = \"home.php\">
-                            <tag>
-                                <tagtag>
-                                    <button type = button>
-                                        About Us
-                                    </button> 
-                                </tagtag>
-                            </tag>
-                        </a>
-                    </li> 
-                    <li>
-                        <a href = \"home.php\">
-                            <tag>
-                                <tagtag>
-                                    <button type = button>
-                                        Login To Distract Me Not
-                                    </button> 
-                                </tagtag>
-                            </tag>
-                        </a>
-                    </li>  
-                </ul> 
-             </nav> 
-          </header>";
-
+	echo "<header> 
+		<nav>
+			<ul>
+				<li>
+					<a href = \"home.php\">
+						<tag>
+							<tagtag>
+								<button type = button>
+									Distract Me Not Industries LLC.
+								</button> 
+							</tagtag>
+						</tag>
+					</a>
+				</li>
+				<li>
+					<a href = \"about-us.php\">
+						<tag>
+							<tagtag>
+								<button type = button>
+									About Us
+								</button> 
+							</tagtag>
+						</tag>
+					</a>
+				</li> 
+				<li>
+					<a href = \"login.php\">
+						<tag>
+							<tagtag>
+								<button type = button>
+									Login To Distract Me Not
+								</button> 
+							</tagtag>
+						</tag>
+					</a>
+				</li>  
+			</ul> 
+		</nav> 
+	</header>";
 ?>
