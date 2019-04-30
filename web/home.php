3820c17ca2bd582dab42a8d4e198ede88860e7f6
George <wie15003@byui.edu>
2019-04-30 16:09:24 -0600

    Added 3 PHP files

---
+++ web/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en-us">
+  <head>
+    <meta charset="utf-8">
+		<title>Home Page</title>
+  </head>
+  <body>
+    <p>
+        <h1>
+            Welcome to Distract Me Not!
+        </h1>
+        
+        You are not logged in.
+    </p>     
+  </body>
+</html>
---
+++ web/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en-us">
+  <head>
+    <meta charset="utf-8">
+		<title>Login</title>
+  </head>
+  <body> 
+    <button type = "button"> Log in as administrator </button>
+    <button type = "button"> Log in as tester </button>    
+  </body>
+</html><!DOCTYPE html>
+<html lang="en">
