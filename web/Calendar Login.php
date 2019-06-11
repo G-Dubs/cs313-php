8e5d9e196648968382f7dbe06d581a50f4c56ad8
George <wie15003@byui.edu>
2019-06-11 15:53:46 -0600

    I can do this in VS Code?!? This is OP!

--- web/Calendar Login.php
+++ web/Calendar Login.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Books</title>
+    <title>Calendar Login</title>
 </head>
 	<body>
 		<h1>Login or create a new account</h1>
