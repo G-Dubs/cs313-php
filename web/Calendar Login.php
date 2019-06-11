c09faf2a0de92075bcd35dc52ea602f43c572b95
George <wie15003@byui.edu>
2019-06-11 15:19:26 -0600

    I can do this in VS Code?!? This is OP!

---
+++ web/Calendar Login.php
@@ -0,0 +1,21 @@
+<?php
+	require ("Connect.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Books</title>
+</head>
+	<body>
+		<h1>Login or create a new account</h1>
+		<hr />
+		</br>
+		<a href="Calendar Display.php">Login</a>
+		</br>
+		<a href="Calendar Account.php">Create</a>
+	</body>
+</html>
