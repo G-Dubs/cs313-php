8bf3e77f1d2b6bde8e5b51a3ea659741b3f8103d
George <wie15003@byui.edu>
2019-06-13 15:24:04 -0600

    I can do this in VS Code?!? This is OP!

--- web/Calendar Account.php
+++ web/Calendar Account.php
@@ -26,7 +26,7 @@
 						Username: 
 					</td>
 					<td colspan="2">
-						<input type="text" name="username" size="16" />
+						<input type="text" name="username" size="15" />
 					</td>
 				</tr>	
 				<tr>
@@ -34,7 +34,7 @@
 						Password: 
 					</td>
 					<td colspan="2">
-						<input type="text" name="password" size="16" />
+						<input type="text" name="password" size="15" />
 					</td>
 				</tr>
 				<tr>
